70ac9cc45ec1c466c46ca052940620a6f47a79d4
Rwilson2855 <rwilson2855@gmail.com>
2025-02-10 20:45:17 -0600

    frankenstein monster code

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Rwilson2855,github.io",
+  base: "/Rwilson2855.github.io",
   plugins: [react()],
 })
