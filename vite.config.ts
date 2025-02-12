ff307aed3554e2ec6519c58fd7bce3c522b3fbdb
Rwilson2855 <rwilson2855@gmail.com>
2025-02-12 17:30:59 -0600

    work

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Rwilson2855.github.io",
   plugins: [react()],
-})
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    // hey! 👋 over here
+    globals: true,
+    setupFiles: './tests/setup.ts',
+}})
