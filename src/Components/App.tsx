333690bc51edd2b88e9aa4be2fd93572f6e80c4f
Rwilson2855 <rwilson2855@gmail.com>
2025-02-12 18:12:50 -0600

    we are live

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -45,4 +45,3 @@ function App() {
 }
 
 export default App;
-
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Rwilson2855.github.io",
+  base: "/",
   plugins: [react()],
   test: {
     // 👋 add the line below to add jsdom to vite
