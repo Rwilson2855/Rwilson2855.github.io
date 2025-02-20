c1da55569ba19e9748b1828da18bb2ee54ee85ba
Rwilson2855 <rwilson2855@gmail.com>
2025-02-19 21:41:45 -0600

    cleaned up test component

--- src/Components/App.test.tsx
+++ src/Components/App.test.tsx
@@ -1,11 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import { describe, it, expect } from 'vitest'
-describe('A truthy statement', () => {
-  it('should be equal to 2', () => {
-    expect(1+1).toEqual(2)
-  })
-})
 describe('App', () => {
   it('renders the App component', () => {
     render(<App />)
